767bf8925b2e5c64d02dc86c012c6a86e4995cf3
cixocs <kazukichihan@gmail.com>
2024-11-20 02:04:43 +0900

    replace unity build files & add headers

--- src/components/UnityWebgl.tsx
+++ src/components/UnityWebgl.tsx
@@ -3,9 +3,9 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 export const UnityWebgl = () => {
   const { unityProvider } = useUnityContext({
     loaderUrl: "/game/happy-harvest.loader.js",
-    dataUrl: "/game/happy-harvest.data.br",
-    frameworkUrl: "/game/happy-harvest.framework.js.br",
-    codeUrl: "/game/happy-harvest.wasm.br",
+    dataUrl: "/game/happy-harvest.data.unityweb",
+    frameworkUrl: "/game/happy-harvest.framework.js.unityweb",
+    codeUrl: "/game/happy-harvest.wasm.unityweb",
   });
 
   return <Unity unityProvider={unityProvider} style={{ width: "100%", height: "100%" }} />;
